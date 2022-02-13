20ffbafa3bcbd13c66542013e5b4a11b1fd5f97c
zereight <definedable@gmail.com>
2022-02-13 15:53:37 +0900

    fix: 기존사용자들 로그인 버그 수정

--- src/App.tsx
+++ src/App.tsx
@@ -89,12 +89,19 @@ const App = () => {
     deleteMutation();
     removeLocalStorage("active");
     removeLocalStorage("refreshToken");
+    removeLocalStorage("accessToken");
   };
 
   const logout = () => {
     removeAccessToken();
     setUser(undefined);
   };
+
+  useEffect(() => {
+    if (!getLocalStorage("accessToken")) {
+      logout();
+    }
+  }, []);
 
   useEffect(() => {
     if (isActiveAccessToken) refetchUser();
