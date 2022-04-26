1f88d41e1055c7b09d6661d5165a08c52399da4a
zereight <definedable@gmail.com>
2022-04-26 22:12:27 +0900

    chore: 배포코드 변경

--- src/constants/domain.ts
+++ src/constants/domain.ts
@@ -1,15 +1,15 @@
 import { BUILD_MODE_TABLE } from "./../types/buildMode";
 
 const REPLY_MODULE_DOMAIN_TABLE = {
-  // production: "https://reply-module.darass.co.kr",
-  production: "https://reply-module.dev.darass.co.kr",
+  production: "https://reply-module.darass.co.kr",
+  // production: "https://reply-module.dev.darass.co.kr",
   development: "https://reply-module.dev.darass.co.kr",
   localhost: "https://localhost:3000"
 } as BUILD_MODE_TABLE;
 
 const MANAGE_DOMAIN_TABLE = {
-  // production: "https://darass.co.kr",
-  production: "https://dev.darass.co.kr/",
+  production: "https://darass.co.kr",
+  // production: "https://dev.darass.co.kr/",
   development: "https://dev.darass.co.kr/",
   localhost: "https://localhost:3001"
 } as BUILD_MODE_TABLE;
@@ -20,5 +20,5 @@ export const REPLY_MODULE_DOMAIN = REPLY_MODULE_DOMAIN_TABLE[process.env.BUILD_M
 
 export const MANAGE_PAGE_DOMAIN = MANAGE_DOMAIN_TABLE[process.env.BUILD_MODE as keyof BUILD_MODE_TABLE];
 
-// export const DEPLOY_SCRIPT_DOMAIN = "https://deploy-script.darass.co.kr";
-export const DEPLOY_SCRIPT_DOMAIN = "https://deploy-script.dev.darass.co.kr";
+export const DEPLOY_SCRIPT_DOMAIN = "https://deploy-script.darass.co.kr";
+// export const DEPLOY_SCRIPT_DOMAIN = "https://deploy-script.dev.darass.co.kr";
